a6c9a744cacefae613f97691d13b266a666beb57
Mahan Fathi <mahan0fathi@gmail.com>
2022-04-19 13:31:43 -0400

    Add loss_L2

--- ebm/__init__.py
+++ ebm/__init__.py
@@ -1,8 +1,8 @@
 from .ebm import EBM
-from .loss import loss_ML, loss_ML_KL
+from .loss import loss_ML, loss_ML_KL, loss_L2
 
 __all__ = ["EBM"]
-__all__ += ["loss_ML", "loss_ML_KL"]
+__all__ += ["loss_ML", "loss_ML_KL", "loss_L2"]
 
 def get_loss_fn(loss_name: str):
     return globals()[loss_name]
