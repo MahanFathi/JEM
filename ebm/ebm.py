216c77883ad92173226e7571f550bee36a462b51
Mahan Fathi <mahan0fathi@gmail.com>
2022-04-20 12:36:54 -0400

    Refactor infer_z_and_a + add langevin_gd

--- ebm/ebm.py
+++ ebm/ebm.py
@@ -8,6 +8,9 @@ from envs.base_env import BaseEnv
 from util.net import make_model
 from util.types import *
 
+# from absl import logging
+# from jax.experimental import host_callback as jhcb
+
 
 class EBM(object):
 
@@ -41,17 +44,18 @@ class EBM(object):
         z: jnp.ndarray
         a: jnp.ndarray
         key: PRNGKey
-        params, s, z, a, key = carry
+        langevin_gd: bool
+        params, s, z, a, key, langevin_gd = carry
 
         alpha = self.cfg.EBM.ALPHA
-        omega = .0
-        if self.cfg.EBM.LANGEVIN_GD:
-            key, langevin_key = jax.random.split(key)
-            omega = jax.random.normal(langevin_key, z.shape) * jnp.sqrt(alpha)
+
+        key, langevin_key = jax.random.split(key)
+        omega = jax.random.normal(langevin_key, z.shape) * jnp.sqrt(alpha)
+        omega *= langevin_gd # TODO: dirty way around jax compiling scan functions
 
         z += -alpha / 2. * self.dedz(params, s, z, a) + omega
 
-        return (params, s, z, a, key), ()
+        return (params, s, z, a, key, langevin_gd), ()
 
 
     @partial(jax.jit, static_argnums=(0, ))
@@ -61,44 +65,105 @@ class EBM(object):
         z: jnp.ndarray
         a: jnp.ndarray
         key: PRNGKey
-        params, s, z, a, key = carry
+        langevin_gd: bool
+        params, s, z, a, key, langevin_gd = carry
 
         alpha = self.cfg.EBM.ALPHA
-        omega = .0
-        if self.cfg.EBM.LANGEVIN_GD:
-            key, langevin_key = jax.random.split(key)
-            omega = jax.random.normal(langevin_key, a.shape) * jnp.sqrt(alpha)
+
+        key, langevin_key = jax.random.split(key)
+        omega = jax.random.normal(langevin_key, a.shape) * jnp.sqrt(alpha)
+        omega *= langevin_gd # TODO: dirty way around jax compiling scan functions
 
         a += -alpha / 2. * self.deda(params, s, z, a) + omega
 
-        return (params, s, z, a, key), ()
+        return (params, s, z, a, key, langevin_gd), ()
 
 
-    def infer_z(self, params: Params, s: jnp.ndarray, z: jnp.ndarray, a: jnp.ndarray, key: PRNGKey):
+    def infer_z(self, params: Params, s: jnp.ndarray, z: jnp.ndarray, a: jnp.ndarray, key: PRNGKey, langevin_gd: bool = None):
 
-        (_, _, z, _, _), _ = jax.lax.scan(
+        if langevin_gd is None:
+            langevin_gd = self.cfg.EBM.LANGEVIN_GD
+
+        (_, _, z, _, _, _), _ = jax.lax.scan(
             self._step_z_grad_descent,
-            (params, s, z, a, key), (), self.cfg.EBM.K)
+            (params, s, z, a, key, langevin_gd), (), self.cfg.EBM.K)
 
         return z
 
 
-    def infer_a(self, params: Params, s: jnp.ndarray, z: jnp.ndarray, a: jnp.ndarray, key: PRNGKey):
+    def infer_a(self, params: Params, s: jnp.ndarray, z: jnp.ndarray, a: jnp.ndarray, key: PRNGKey, langevin_gd: bool = None):
 
-        (_, _, _, a, _), _ = jax.lax.scan(
+        if langevin_gd is None:
+            langevin_gd = self.cfg.EBM.LANGEVIN_GD
+
+        (_, _, _, a, _, _), _ = jax.lax.scan(
             self._step_a_grad_descent,
-            (params, s, z, a, key), (), self.cfg.EBM.K)
+            (params, s, z, a, key, langevin_gd), (), self.cfg.EBM.K)
 
         return a
 
 
     def scan_to_infer_multiple_a(self, carry, x: StepData):
-        params, z, key = carry
+        params, z, key, langevin_gd = carry
         s = x.observation
         a_init = x.action
 
         key, key_infer_a = jax.random.split(key)
 
-        a = self.infer_a(params, s, z, a_init, key_infer_a)
+        a = self.infer_a(params, s, z, a_init, key_infer_a, langevin_gd)
 
-        return (params, z, key), a
+        return (params, z, key, langevin_gd), a
+
+
+
+def infer_z_and_a(params: Params, data: StepData, key: PRNGKey, cfg: FrozenConfigDict, ebm: EBM, langevin_gd: bool = None):
+    """
+    Infers the option from the first transition in the data and
+    then predicts future actions based on the inferred option.
+
+    data: (batch_size, horizon, dim)
+
+    returns:
+        z: (batch_size, option_size)
+        a: (batch_size, horizon - 1, action_size)
+    """
+
+    if langevin_gd is None:
+        langevin_gd = cfg.EBM.LANGEVIN_GD
+
+    batch_size, horizon, action_size = data.action.shape
+    option_size = cfg.EBM.OPTION_SIZE
+
+    # infer z from first state-action
+    key, key_init_z, key_infer_z = jax.random.split(key, 3)
+    z_init = jax.random.normal(key_init_z, (batch_size, option_size))
+    z = ebm.infer_z(
+        params, data.observation[:, 0, :],
+        z_init, data.action[:, 0, :], key_infer_z,
+    )
+
+    # infer actions based on the inferred option
+    key, key_init_a, key_infer_a = jax.random.split(key, 3)
+    a_init = jax.random.normal(key_init_a, (horizon - 1, batch_size, action_size))
+    _, a = jax.lax.scan(
+        ebm.scan_to_infer_multiple_a, (params, z, key, langevin_gd),
+        StepData(
+            observation=data.observation.swapaxes(0, 1)[1:],
+            action=a_init,
+        ),
+    )
+
+    a = a.swapaxes(0, 1)
+
+    # if cfg.DEBUG:
+        # debug_log_fn = lambda x: logging.log(logging.DEBUG, "\n***Inferred Actions***: \n{}".format(x))
+        # jhcb.id_tap(debug_log_fn, a)
+        # debug_log_fn = lambda x: logging.log(logging.DEBUG, "\n***Inferred Options***: \n{}".format(x))
+        # jhcb.id_tap(debug_log_fn, z)
+
+        # print("\ninferred options: \n")
+        # jhcb.id_tap(print, z)
+        # print("\ninferred actions: \n")
+        # jhcb.id_tap(print, a)
+
+    return z, a
--- ebm/loss.py
+++ ebm/loss.py
@@ -5,65 +5,13 @@ from jax import numpy as jnp
 from ml_collections import FrozenConfigDict
 
 from ebm import EBM
+from ebm.ebm import infer_z_and_a
 from util.types import *
-
-# from absl import logging
-# from jax.experimental import host_callback as jhcb
 
 
 # helper functions
 def _soft_plus(x):
     return jnp.log(1. + jnp.exp(x))
-
-
-def _infer_z_and_a(params: Params, data: StepData, key: PRNGKey, cfg: FrozenConfigDict, ebm: EBM):
-    """
-    Infers the option from the first transition in the data and
-    then predicts future actions based on the inferred option.
-
-    data: (batch_size, horizon, dim)
-
-    returns:
-        z: (batch_size, option_size)
-        a: (batch_size, horizon - 1, action_size)
-    """
-
-    batch_size, horizon, action_size = data.action.shape
-    option_size = cfg.EBM.OPTION_SIZE
-
-    # infer z from first state-action
-    key, key_init_z, key_infer_z = jax.random.split(key, 3)
-    z_init = jax.random.normal(key_init_z, (batch_size, option_size))
-    z = ebm.infer_z(
-        params, data.observation[:, 0, :],
-        z_init, data.action[:, 0, :], key_infer_z,
-    )
-
-    # infer actions based on the inferred option
-    key, key_init_a, key_infer_a = jax.random.split(key, 3)
-    a_init = jax.random.normal(key_init_a, (horizon - 1, batch_size, action_size))
-    _, a = jax.lax.scan(
-        ebm.scan_to_infer_multiple_a, (params, z, key),
-        StepData(
-            observation=data.observation.swapaxes(0, 1)[1:],
-            action=a_init,
-        ),
-    )
-
-    a = a.swapaxes(0, 1)
-
-    # if cfg.DEBUG:
-        # debug_log_fn = lambda x: logging.log(logging.DEBUG, "\n***Inferred Actions***: \n{}".format(x))
-        # jhcb.id_tap(debug_log_fn, a)
-        # debug_log_fn = lambda x: logging.log(logging.DEBUG, "\n***Inferred Options***: \n{}".format(x))
-        # jhcb.id_tap(debug_log_fn, z)
-
-        # print("\ninferred options: \n")
-        # jhcb.id_tap(print, z)
-        # print("\ninferred actions: \n")
-        # jhcb.id_tap(print, a)
-
-    return z, a
 
 
 def _calc_action_distance(a, a_pred, discount):
@@ -76,7 +24,7 @@ def _calc_loss_ml_kl_l2(params: Params, data: StepData, key: PRNGKey, cfg: Froze
     #   z: (batch_size, option_size)
     #   a: (batch_size, horizon - 1, action_size)
     key, key_infer = jax.random.split(key)
-    z, a = _infer_z_and_a(params, data, key, cfg, ebm)
+    z, a = infer_z_and_a(params, data, key, cfg, ebm)
 
     horizon = a.shape[1] + 1
     z = jnp.stack([z] * (horizon - 1), axis=1) # (batch_size, horizon - 1, option_size)
@@ -110,7 +58,7 @@ def loss_L2(params: Params, data: StepData, key: PRNGKey, cfg: FrozenConfigDict,
     #   z: (batch_size, option_size)
     #   a: (batch_size, horizon - 1, action_size)
     key, key_infer = jax.random.split(key)
-    _, a = _infer_z_and_a(params, data, key, cfg, ebm)
+    _, a = infer_z_and_a(params, data, key, cfg, ebm)
 
     loss_l2 = _calc_action_distance(data.action[:, 1:, :], a, cfg.TRAIN.EBM.DISCOUNT)
     return loss_l2, {
@@ -128,6 +76,7 @@ def loss_ML(params: Params, data: StepData, key: PRNGKey, cfg: FrozenConfigDict,
 def loss_ML_KL(params: Params, data: StepData, key: PRNGKey, cfg: FrozenConfigDict, ebm: EBM):
     losses = _calc_loss_ml_kl_l2(params, data, key, cfg, ebm)
     loss_ml = losses["loss_ml"]
+    loss_kl = losses["loss_kl"]
     loss = loss_ml + cfg.TRAIN.EBM.LOSS_KL_COEFF * loss_kl
     return loss, {**{"loss": loss}, **losses}
 
