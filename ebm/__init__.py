98d4c5dce1aed3ab91bc85281dde0f146f09d935
Mahan Fathi <mahan0fathi@gmail.com>
2022-05-17 15:31:36 -0400

    Add loss_contrastive imports

--- ebm/__init__.py
+++ ebm/__init__.py
@@ -1,9 +1,9 @@
 from .ebm import EBM
-from .loss import loss_ML, loss_ML_KL, loss_L2, loss_L2_KL
+from .loss import loss_ML, loss_ML_KL, loss_L2, loss_L2_KL, loss_contrastive
 from .loss import eval_action_l2
 
 __all__ = ["EBM"]
-__all__ += ["loss_ML", "loss_ML_KL", "loss_L2", "loss_L2_KL"]
+__all__ += ["loss_ML", "loss_ML_KL", "loss_L2", "loss_L2_KL", "loss_contrastive"]
 __all__ += ["eval_action_l2"]
 
 def get_loss_fn(loss_name: str):
